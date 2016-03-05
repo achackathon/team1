782e26a48f3721da0de3009be5dabe507d411328
Phillipe Caixeta <caixeta.phillipe@gmail.com>
2016-03-05 10:41:33 -0300

    Estrutura do Banco de Dados

---
+++ bd/bd.sql
@@ -0,0 +1,66 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `webgotinha` DEFAULT CHARACTER SET utf8 ;
+USE `webgotinha` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(50) NOT NULL,
+  `senha` VARCHAR(45) NOT NULL,
+  `dataNasc` DATE NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `sexo` CHAR(1) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Vacina`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Vacina` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `dose` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Usuario_has_Vacina`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Usuario_has_Vacina` (
+  `id_usuario` INT NOT NULL,
+  `id_vacina` INT NOT NULL,
+  PRIMARY KEY (`id_usuario`, `id_vacina`),
+  INDEX `fk_Usuario_has_Vacina_Vacina1_idx` (`id_vacina` ASC),
+  INDEX `fk_Usuario_has_Vacina_Usuario_idx` (`id_usuario` ASC),
+  CONSTRAINT `fk_Usuario_has_Vacina_Usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `mydb`.`Usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Usuario_has_Vacina_Vacina1`
+    FOREIGN KEY (`id_vacina`)
+    REFERENCES `mydb`.`Vacina` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
