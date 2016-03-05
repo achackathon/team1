aa002a941b20d33435e4c055779de8876cf73740
leozincar <leozincar@yahoo.com.br>
2016-03-05 15:27:41 -0300

    scripts carga

---
+++ bd/carga.sql
@@ -0,0 +1,44 @@
+/* TABELA - Vacina */
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina BCG', 'Única dose', 'Contra a Tuberculose, é realizada em geral ao nascer em uma única dose, e é oferecida em postos de saúde.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina contra Hepatite B', '3 doses', 'É aplicada em crianças e adultos. São três doses: uma ao nascer, a segunda entre 1 e 2 meses de vida e a terceira ao 6 meses de idade. Essa vacina também está disponível em postos de saúde para as crianças.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina contra Hepatite A', 'reforço', 'É recomendada para crianças a partir de 1 ano e para pessoas que viajam para áreas onde a Hepatite A é muito frequente, como a região Norte do Brasil e países tropicais subdesenvolvidos. Protege por aproximadamente 25 anos.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina Tríplice Bacteriana', '3 doses', 'Combate a difteria, a coqueluche e o tétano. São aplicadas em três doses, aos 2, 4 e 6 meses de vida, tendo dois reforços aos 15 meses e entre 4 e 6 anos.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina Tríplice Viral', '1 reforço', 'Protege contra o sarampo, a caxumba e a rubéola. Deve ser aplicada aos 15 meses de idade e o reforço entre 4 e 10 anos.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina contra Varicela', '1 reforço', 'A varicela, conhecida como catapora, é uma doença infecciosa altamente contagiosa. A vacina é recomendada após o primeiro ano de idade, com reforço entre 4 e 10 anos de idade.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina Anti-Meningocóccica C conjugada', '', 'Previne crianças e adultos de contraírem meningite meningocócica, que atinge o cérebro, e a meningococemia, infecção generalizada no sangue. Nas crianças são aplicadas duas doses e um reforço.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina contra Poliomielite', '3 doses', 'Três doses, aos 2, 4 e 6 meses e um reforço aos 15 meses. Apesar de não haver novos casos, a vacina continua sendo muito importante.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina contra Rotavírus', '2 doses', 'Esse vírus causa diarréia, principalmente em crianças menores de 2 anos, podendo atingir também adultos. A vacina contra infecções por Rotavírus, recentemente licenciada para o uso no Brasil, deve ser aplicada em crianças com idade entre 6 semanas e 6 meses de idade. São duas doses aos 2 e 4 meses.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina contra Influenza (gripe)', 'reforço', 'Deve ser administrada após os 6 meses de idade. Vacinações anuais são indicadas, uma vez que a composição das vacinas é alterada em função das espécies de vírus. Deve ser tomada por crianças, adultos e idosos. Nas crianças com idade inferior a 9 anos, deverá ser consultado um médico.')
+
+insert into Vacina (nome, dose, descrcao)
+values ('Vacina contra Febre Amarela', 'reforço', 'A doença é transmitida por mosquitos e ocorre exclusivamente na América Central, na América do Sul e na África. Locais onde ocorreram recentes catástrofes naturais exigem a vacina. A vacinação é indicada para crianças a partir de 9 meses de idade e confere imunidade por, pelo menos, 10 anos (provavelmente por toda a vida). A vacina é gratuita na rede pública.')
+
+
+
+/* TABELA - Usuário */
+insert into Usuario (nome, senha, dataNasc, email, sexo)
+values ('Fulano', '123', '1990-05-10', 'fulano@email.com', 'm')
+
+insert into Usuario (nome, senha, dataNasc, email, sexo)
+values ('Maria', '123', '2000-11-02', 'maria@email.com', 'f')
+
+
